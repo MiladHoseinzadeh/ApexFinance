282cb5ee12a4b839d7971f6dbeb4edf56352c7d3
Milad <milad.hz75@gmail.com>
2024-07-23 19:50:22 +0330

    Feat: Added transaction list page templates, view and url

--- finance/urls.py
+++ finance/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from finance.views import index
+from finance.views import index, transactions_list
 
 
 app_name="finance"
 
 urlpatterns = [
-	path("", index, name="index")
+	path("", index, name="index"),
+	path("transactions/", transactions_list, name="transactions_list"),
 ]
--- finance/views.py
+++ finance/views.py
@@ -1,5 +1,18 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from finance.models import Transaction
 
 
 def index(request):
     return render(request, "finance/index.html")
+
+
+@login_required
+def transactions_list(request):
+    transactions = Transaction.objects.filter(user=request.user)
+    
+    context = {
+        "transactions":transactions
+    }
+    return render(request, "finance/transactions_list.html", context)
